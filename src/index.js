4461294765fb7e9bbe6bb620b7ab8a6c888e89d3
SirriRyisa <sirriryisa@gmail.com>
2022-01-27 11:24:23 +0100

    Fixed linters

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "./style.css"
+import './style.css'
 
-const mainContainer = document.querySelector(".main-container");
+// const mainContainer = document.querySelector(".main-container");
 
-let toDo = [];
+// let toDo = [];
