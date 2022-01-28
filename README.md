780aafe51285dbc2f875a0345b8c4cf92eede1f4
SirriRyisa <sirriryisa@gmail.com>
2022-01-28 15:58:27 +0100

    basic layout rendering with html

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Webpack-Repo
+
+Building a Todo list with JavaScript where a user can check a box
--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import './style.css';
 
-// const mainContainer = document.querySelector(".main-container");
+// select the Element
+const mainContainer = document.querySelector(".main-container");
+const input = document.getElementById('#sports');
+const label = document.getElementById('#item');
+const clearList = document.querySelector(".clear-list");
+const listItem = document.querySelector('.list-item');
+const addToDo = document.querySelector('.add-todo');
+const contentToDo = document.getElementById('lists')
 
-// let toDo = [];
+// classes names
+
+function myToDo(toDo) {
+  const item = `
+          <li class="list-item">
+            <input type="checkbox" id="sports" name="Sports" value="Sports">
+            <label for="Sports" id="item"> ${toDo}<span><i class="fas fa-ellipsis-v" job="delete" id="0"></i></span></label><br>
+           </li>
+  `;
+  const position = 'beforeend';
+
+  contentToDo.insertAdjacentHTML(position, item);
+}
+
+myToDo("Learn JavaScript");
+
+// document.addEventListener("keyup", function(event) {
+//   if(event.keyboardEvent.keyCode==13) {
+
+//   }
+// })
+
