6788e0d58d2a2d68e7a4af433d108983502d7862
SirriRyisa <sirriryisa@gmail.com>
2022-01-29 01:43:53 +0100

    Fixed linters errors on HTML, CESS and JS files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import './style.css';
 
 // select the Element
-const mainContainer = document.querySelector(".main-container");
-const input = document.getElementById('#sports');
-const label = document.getElementById('#item');
-const clearList = document.querySelector(".clear-list");
-const listItem = document.querySelector('.list-item');
-const addToDo = document.querySelector('.add-todo');
+const toDoo = document.querySelector('.add-todo');
 const contentToDo = document.getElementById('lists')
 
 // classes names
@@ -14,20 +9,28 @@ const contentToDo = document.getElementById('lists')
 function myToDo(toDo) {
   const item = `
           <li class="list-item">
-            <input type="checkbox" id="sports" name="Sports" value="Sports">
-            <label for="Sports" id="item"> ${toDo}<span><i class="fas fa-ellipsis-v" job="delete" id="0"></i></span></label><br>
+            <input type="checkbox" class="sportss" name="Sports" value="Sports">
+            <label for="Sports" id="item"> ${toDo}<span class="removee"><i class="fas fa-ellipsis-v""></i></span></label><br>
            </li>
   `;
   const position = 'beforeend';
 
-  contentToDo.insertAdjacentHTML(position, item);
+  contentToDo.insertAdjacentHTML(position, item); 
 }
 
-myToDo("Learn JavaScript");
+// add item to list when user hit enter
 
-// document.addEventListener("keyup", function(event) {
-//   if(event.keyboardEvent.keyCode==13) {
+toDoo.addEventListener('keyup', function(event) {
+   if(event.keyCode == 13){
+      const toDo = toDoo.value;
 
-//   }
-// })
+      if(toDo){
+        myToDo(toDo);
+      }
+      toDoo.value = '';
+   }
+ })
 
+myToDo.addEventListener('click', (toDo) => {
+  myToDo.style.textdecoration = 'line-through';
+});
